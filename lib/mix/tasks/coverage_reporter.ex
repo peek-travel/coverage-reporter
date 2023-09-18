df369f436b39fb5894c5e1eb6766e1c5d1f033d1
Brian Berlin <bri@nberl.in>
2023-09-18 08:15:26 -0500

    Update coverage_reporter.ex

--- lib/mix/tasks/coverage_reporter.ex
+++ lib/mix/tasks/coverage_reporter.ex
@@ -1,191 +1,392 @@
 defmodule Mix.Tasks.CoverageReporter do
-  @moduledoc false
+  @shortdoc "Analyzes coverage from lcov files and reports to GitHub"
+  @moduledoc """
+  Analyzes coverage from lcov files and reports to GitHub
+
+  ### How to read an lcov file.
+
+  Source: https://github.com/linux-test-project/lcov/issues/113#issuecomment-762335134
+
+    - TN: test name
+    - SF: source file path
+    - FN: line number,function name
+    - FNF:  number functions found
+    - FNH: number hit
+    - BRDA: branch data: line, block, (expressions,count)+
+    - BRF: branches found
+    - DA: line number, hit count
+    - LF: lines found
+    - LH:  lines hit.
+
+  However, The LCOV files produced by excoveralls only include SF, DA, LF, LH, and end_of_record lines.
+  """
+
   use Mix.Task
 
-  alias Mix.Tasks.Coveralls
-
-  @shortdoc "Runs the test suite and generates a coverage reports"
-  @preferred_cli_env :test
+  @switches [
+    app: :string,
+    github_token: :string,
+    pull_number: :integer,
+    repository: :string,
+    head_branch: :string
+  ]
 
   def run(args) do
-    switches = [
-      base_branch: :string,
-      head_branch: :string,
-      repository: :string,
-      organization: :string,
-      github_token: :string,
-      changed_files: :string,
-      app: :string
-    ]
+    Mix.Task.run("compile")
 
-    {opts, _, _} = OptionParser.parse(args, switches: switches)
+    {opts, _, _} = OptionParser.parse(args, strict: @switches)
 
-    run_tests()
-    post(opts)
+    pull_number = Keyword.fetch!(opts, :pull_number)
+    repository = Keyword.fetch!(opts, :repository)
+    github_token = Keyword.fetch!(opts, :github_token)
+    head_branch = Keyword.fetch!(opts, :head_branch)
+
+    changed_files = get_changed_files(repository, pull_number, github_token)
+    {total, module_results} = get_coverage_from_lcov_files()
+    changed_module_results = module_results_for_changed_files(module_results, changed_files)
+
+    title = "Code Coverage for ##{pull_number}"
+    badge = create_total_coverage_badge(total)
+    coverage_by_file = get_coverage_by_file(changed_module_results)
+    annotations = create_annotations(changed_module_results, changed_files)
+
+    summary = Enum.join(["#### #{title}", badge, coverage_by_file], "\n\n")
+
+    params = %{
+      name: "Code Coverage",
+      head_sha: head_branch,
+      status: "completed",
+      conclusion: get_conclusion(total),
+      output: %{
+        title: title,
+        # Maximum length for summary and text is 65535 characters.
+        summary: String.slice(Enum.join([badge, coverage_by_file], "\n\n"), 0, 65_535),
+        # 50 is the max number of annotations allowed by GitHub.
+        annotations: Enum.take(annotations, 50)
+      }
+    }
+
+    github_request(:post, "#{repository}/check-runs", github_token, params)
+    create_or_update_review_comment(repository, pull_number, summary, github_token)
+
+    :ok
   end
 
-  defp run_tests() do
-    Coveralls.do_run([], type: "json")
+  defp create_total_coverage_badge(total) do
+    params = "logo=elixir&logoColor=purple&labelColor=white"
+    url = "Total%20Coverage-#{format_percentage(total)}%25-#{get_conclusion_color(total)}"
+    "![Total Coverage](https://img.shields.io/badge/#{url}?#{params})"
   end
 
-  defp coverage_data(opts) do
-    app = Keyword.get(opts, :app)
-    coveralls_path = "cover/excoveralls.json"
+  defp module_results_for_changed_files(module_results, changed_files) do
+    changed_file_names = Enum.map(changed_files, fn %{file: file} -> file end)
 
-    cond do
-      not is_nil(app) ->
-        do_get_coverage_data("#{app}/#{coveralls_path}")
+    Enum.filter(module_results, fn {_, module_path, _} ->
+      Enum.any?(changed_file_names, fn changed_file_name ->
+        String.ends_with?(changed_file_name, module_path)
+      end)
+    end)
+  end
 
-      Mix.Project.umbrella?() ->
-        Enum.flat_map(Mix.Project.apps_paths(), fn {_app, path} ->
-          excoveralls_path = "#{path}/cover/excoveralls.json"
-          if File.exists?(excoveralls_path) do
-            excoveralls_path
-            |> do_get_coverage_data()
-            |> update_in([Access.all(), "name"], &"#{path}/#{&1}")
-          else
-            []
-          end
+  defp get_coverage_by_file([]), do: nil
+
+  defp get_coverage_by_file(module_results) do
+    padding =
+      module_results
+      |> Enum.max_by(fn {_, name, _} -> String.length(name) end)
+      |> elem(1)
+      |> String.length()
+
+    Enum.join(
+      [
+        "**Coverage by file**\n",
+        "| Percentage | #{format_name("Module", padding)} |",
+        "| ---------- | #{String.duplicate("-", padding)} |",
+        "#{create_module_results(module_results, padding)}"
+      ],
+      "\n"
+    )
+  end
+
+  defp get_coverage_from_lcov_files do
+    lcov_paths = Path.wildcard("cover/**/*lcov.info")
+    table = :ets.new(__MODULE__, [:set, :private])
+
+    module_results =
+      Enum.flat_map(lcov_paths, fn path ->
+        path
+        |> File.stream!()
+        |> Stream.map(&String.trim(&1))
+        |> Stream.chunk_by(&(&1 == "end_of_record"))
+        |> Stream.reject(&(&1 == ["end_of_record"]))
+        |> Stream.map(fn record -> process_lcov_record(table, record) end)
+      end)
+
+    covered = :ets.select_count(table, [{{{:_, :_}, true}, [], [true]}])
+    not_covered = :ets.select_count(table, [{{{:_, :_}, false}, [], [true]}])
+    total = percentage(covered, not_covered)
+
+    {total, module_results}
+  end
+
+  defp process_lcov_record(table, record) do
+    "SF:" <> path = Enum.find(record, &String.starts_with?(&1, "SF:"))
+
+    coverage_by_line =
+      record
+      |> Enum.filter(fn line -> String.starts_with?(line, "DA:") end)
+      |> Enum.map(fn "DA:" <> value ->
+        [line_number, count] =
+          value
+          |> String.split(",")
+          |> Enum.map(&String.to_integer(&1))
+
+        line_number = line_number - 1
+
+        if count == 0 do
+          :ets.insert(table, {{path, line_number}, false})
+        else
+          Enum.each(1..count, fn _ -> :ets.insert(table, {{path, line_number}, true}) end)
+        end
+
+        {line_number, count}
+      end)
+
+    covered = :ets.select_count(table, [{{{path, :_}, true}, [], [true]}])
+    not_covered = :ets.select_count(table, [{{{path, :_}, false}, [], [true]}])
+    {percentage(covered, not_covered), path, coverage_by_line}
+  end
+
+  defp get_changed_files(repository, pull_number, github_token) do
+    {:ok, 200, files} = github_request_all("#{repository}/pulls/#{pull_number}/files", github_token)
+
+    files
+    |> Enum.reject(&String.equivalent?(&1["status"], "removed"))
+    |> Enum.filter(&String.ends_with?(&1["filename"], ".ex"))
+    |> Enum.map(fn file -> %{file: file["filename"], changed_lines: extract_changed_lines(file["patch"])} end)
+  end
+
+  defp create_module_results([], padding), do: "| No Changes | #{String.duplicate(" ", padding)} |"
+
+  defp create_module_results(module_results, padding) do
+    Enum.map_join(module_results, "\n", &display(elem(&1, 0), elem(&1, 1), padding))
+  end
+
+  defp percentage(0, 0), do: 100.0
+  defp percentage(covered, not_covered), do: covered / (covered + not_covered) * 100
+
+  defp display(percentage, name, padding) do
+    "| #{format_name(format_percentage(percentage) <> "%", 10)} | #{format_name(name, padding)} |"
+  end
+
+  defp format_percentage(number) do
+    number =
+      number
+      |> Float.round(2)
+      |> Float.to_string()
+
+    number
+  end
+
+  defp format_name(name, padding) when is_binary(name) do
+    String.pad_trailing(name, padding, " ")
+  end
+
+  defp get_conclusion(total) do
+    if total >= 80 do
+      "success"
+    else
+      "neutral"
+    end
+  end
+
+  defp get_conclusion_color(total) do
+    if total >= 80 do
+      "green"
+    else
+      "yellow"
+    end
+  end
+
+  defp create_or_update_review_comment(repository, pull_number, summary, github_token) do
+    {:ok, 200, reviews} = github_request_all("#{repository}/pulls/#{pull_number}/reviews", github_token)
+
+    review = Enum.find(reviews, &(&1["body"] =~ "Code Coverage for ##{pull_number}"))
+
+    if is_nil(review) do
+      github_request(:post, "#{repository}/pulls/#{pull_number}/reviews", github_token, %{
+        body: summary,
+        event: "COMMENT"
+      })
+    else
+      github_request(:put, "#{repository}/pulls/#{pull_number}/reviews/#{review["id"]}", github_token, %{
+        body: summary
+      })
+    end
+  end
+
+  defp create_annotations(changed_module_results, changed_files) do
+    Enum.flat_map(changed_module_results, fn {_percentage, module_path, coverage_by_line} ->
+      %{file: file, changed_lines: changed_lines} =
+        Enum.find(changed_files, fn %{file: file} -> String.ends_with?(file, module_path) end)
+
+      source_code_lines =
+        file
+        |> File.read!()
+        |> String.split("\n")
+        |> Enum.with_index(0)
+        |> Enum.map(fn {line, index} -> [nil, line, index] end)
+
+      source_code =
+        coverage_by_line
+        |> Enum.reduce(source_code_lines, fn {line_number, count}, source_code_lines ->
+          List.update_at(source_code_lines, line_number, &[count, Enum.at(&1, 1), Enum.at(&1, 2)])
         end)
+        |> Enum.map(&add_source_code_line/1)
 
-      true ->
-        do_get_coverage_data("cover/excoveralls.json")
-    end
+      coverage_by_line
+      |> Enum.filter(fn {_line_number, count} -> count == 0 end)
+      |> Enum.map(fn {line_number, _} -> line_number end)
+      |> Enum.reduce(_groups = [], &add_line_to_groups/2)
+      |> Enum.reduce(_annotations = [], fn line_number_group, annotations ->
+        end_line = List.first(line_number_group)
+        start_line = List.last(line_number_group)
+        add_annotation? = Enum.any?(changed_lines, &(&1 >= start_line and &1 <= end_line))
+
+        if add_annotation? do
+          annotation =
+            Map.merge(
+              %{
+                title: "Code Coverage",
+                start_line: start_line,
+                end_line: end_line,
+                annotation_level: "warning",
+                path: file
+              },
+              create_annotation_message(start_line, end_line, source_code)
+            )
+
+          [annotation] ++ annotations
+        else
+          annotations
+        end
+      end)
+    end)
   end
 
-  defp do_get_coverage_data(path) do
-    if File.exists?(path) do
-      path
-      |> File.read!()
-      |> Jason.decode!()
-      |> Map.get("source_files")
-      |> update_in([Access.all(), "source"], &String.split(&1, "\n"))
-    else
-      []
-    end
+  defp add_source_code_line([nil, line, line_number]) do
+    "#{String.duplicate(" ", 5)} #{String.pad_trailing("#{line_number}", 3)} #{line}"
   end
 
-  def get_changed_files(opts) do
-    if opts[:changed_files] do
-      String.split(opts[:changed_files], " ", trim: true)
-    else
-      base_branch = Keyword.fetch!(opts, :base_branch)
-      {changed_files, 0} = System.cmd("git", ["diff", "--name-only", "--diff-filter=ACMRT", "origin/#{base_branch}", "HEAD"])
-      String.split(changed_files, "\n", trim: true)
-    end
+  defp add_source_code_line([count, line, line_number]) do
+    "#{String.pad_trailing("#{count}", 5, ".")} #{String.pad_trailing("#{line_number}", 3)} #{line}"
   end
 
-  def read_coveralls_export(opts) do
-    opts[:coverage_data] || coverage_data(opts)
-    |> Enum.filter(&Enum.member?(get_changed_files(opts), &1["name"]))
-  end
-
-  def build_params(opts) do
-    source_files = read_coveralls_export(opts)
-    messages = Enum.flat_map(source_files, &create_groups/1)
+  defp create_annotation_message(start_line, end_line, source_code) do
+    source_code =
+      source_code
+      |> Enum.slice((start_line - 5)..(end_line + 5))
+      |> Enum.join("\n")
 
     %{
-      owner: opts[:organization],
-      repo: opts[:repository],
-      name: name(opts),
-      head_sha: opts[:head_branch],
-      status: "completed",
-      conclusion: conclusion(source_files),
-      output: %{
-        title: "Code Coverage Report",
-        summary: "Below is a summary of the missing coverages from the code coverage report",
-        text: build_text_output(messages)
-      }
+      message: "Lines #{start_line} to #{end_line} are not covered by tests.",
+      raw_details: source_code
     }
   end
 
-  defp name(opts) do
-    app = Keyword.get(opts, :app)
+  defp add_line_to_groups(line_number, groups) do
+    cond do
+      Enum.empty?(groups) ->
+        [[line_number]]
 
-    if not is_nil(app) do
-      "Code Coverage - #{app}"
-    else
-      "Code Coverage"
+      [current_group | remaining_groups] = groups ->
+        previous_line_number = List.first(current_group)
+
+        if line_number - previous_line_number < 4 do
+          [[line_number] ++ current_group] ++ remaining_groups
+        else
+          [[line_number]] ++ groups
+        end
     end
   end
 
-  defp build_text_output(messages) do
-    Enum.map_join(messages, "\n\n", fn message ->
-      """
-      <details>
-        <summary>#{message.name}</summary>
+  defp extract_changed_lines(patch) do
+    patch
+    |> String.split("\n")
+    |> Enum.reduce({nil, []}, fn line, {current_line, changes} ->
+      case Regex.scan(~r/@@ -\d+,?\d* \+(\d+),?\d* @@/, line) do
+        [[_ | [start_line]]] ->
+          {String.to_integer(start_line), changes}
 
-        ```diff
-        #{message.diff}
-        ```
-      </details>
-      """
+        _ ->
+          case line do
+            "+" <> _ -> {current_line + 1, [current_line | changes]}
+            "-" <> _ -> {current_line, changes}
+            " " <> _ -> {current_line + 1, changes}
+            _ -> {nil, changes}
+          end
+      end
     end)
+    |> elem(1)
+    |> Enum.reverse()
   end
 
-  defp conclusion(source_files) do
-    source_files
-    |> get_in([Access.all(), "coverage"])
-    |> List.flatten()
-    |> Enum.find(&(&1 == 0))
-    |> case do
-      nil -> "success"
-      _ -> "failure"
+  defp github_request_all(path, github_token, params \\ %{page: 1}, accumulator \\ []) do
+    case github_request(:get, path, github_token, params) do
+      {:ok, 200, []} ->
+        {:ok, 200, accumulator}
+
+      {:ok, 200, results} ->
+        params = Map.put(params, :page, params[:page] + 1)
+        github_request_all(path, github_token, params, results ++ accumulator)
+
+      {:ok, status_code, result} ->
+        {:ok, status_code, result}
+
+      {:error, reason} ->
+        {:error, reason}
     end
   end
 
-  defp create_groups(%{"coverage" => coverage, "name" => name, "source" => source}) do
-    coverage
-    |> Enum.with_index()
-    |> Enum.drop_while(&is_nil(elem(&1, 0)))
-    |> Enum.reverse()
-    |> Enum.drop_while(&is_nil(elem(&1, 0)))
-    |> Enum.reverse()
-    |> Enum.reduce([[]], fn {num, index}, [current_group | rest] ->
-      [Enum.concat([{num, index, Enum.at(source, index)}], current_group) | rest]
-    end)
-    |> Enum.map(&Enum.reverse/1)
-    |> Enum.reverse()
-    |> Enum.reject(&Enum.empty?/1)
-    |> Enum.map(fn group ->
-      start_line = group |> List.first() |> elem(1)
-      end_line = group |> List.last() |> elem(1)
-
-      diff =
-        Enum.map_join(group, "\n", fn
-          {nil, _, source} -> "# #{source}"
-          {0, _, source} -> "- #{source}"
-          {1, _, source} -> "! #{source}"
-          {coverage, _, source} when coverage > 1 -> "+ #{source}"
-        end)
-
-      %{name: name, start_line: start_line, end_line: end_line, diff: diff}
-    end)
-  end
-
-  defp post(opts) do
+  defp github_request(method, path, github_token, params) do
     :inets.start()
     :ssl.start()
 
-    url = 'https://api.github.com/repos/#{opts[:organization]}/#{opts[:repository]}/check-runs'
+    url = 'https://api.github.com/repos/#{path}'
+
     headers = [
-      {'Authorization', 'Bearer #{Keyword.fetch!(opts, :github_token)}'},
+      {'Authorization', 'Bearer #{github_token}'},
       {'Accept', 'application/vnd.github+json'},
       {'X-GitHub-Api-Version', '2022-11-28'},
       {'User-Agent', 'CoverageReporter'}
     ]
-    content_type = 'application/json'
-    body = Jason.encode!(build_params(opts))
-    http_request_opts = [
-      ssl: [
-        verify: :verify_peer,
-        cacerts: :public_key.cacerts_get(),
-        customize_hostname_check: [
-          match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
-        ]
+
+    request =
+      case method do
+        :get ->
+          {'#{url}?#{URI.encode_query(params)}', headers}
+
+        :post ->
+          {url, headers, 'application/json', Jason.encode!(params)}
+
+        :put ->
+          {url, headers, 'application/json', Jason.encode!(params)}
+      end
+
+    ssl = [
+      verify: :verify_peer,
+      cacerts: :public_key.cacerts_get(),
+      customize_hostname_check: [
+        match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
       ]
     ]
 
-    :httpc.request(:post, {url, headers, content_type, body}, http_request_opts, [])
+    case :httpc.request(method, request, [ssl: ssl], []) do
+      {:ok, {{_http_version, status_code, _response_string}, _headers, body}} ->
+        {:ok, status_code, Jason.decode!(body)}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
   end
 end
