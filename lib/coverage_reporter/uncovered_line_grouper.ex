f1dc82bc5754d4cf1e7d48bc686a1a4e9d9f83f3
Brian Berlin <bri@nberl.in>
2025-10-01 22:33:11 -0500

    dialyzer was right

--- lib/coverage_reporter/uncovered_line_grouper.ex
+++ lib/coverage_reporter/uncovered_line_grouper.ex
@@ -1,12 +1,7 @@
 defmodule CoverageReporter.UncoveredLineGrouper do
   @moduledoc false
 
-  @doc """
-  Groups consecutive uncovered lines into single annotations.
-
-  Returns a list of line number groups, where each group represents
-  consecutive uncovered lines that should be annotated together.
-  """
+  # Groups consecutive uncovered lines into single annotations.
   def group_lines(source_lines, coverage_map) do
     source_lines
     |> Enum.map(fn {line_content, line_number} ->
@@ -20,6 +15,7 @@ defmodule CoverageReporter.UncoveredLineGrouper do
     |> Enum.reverse()
   end
 
+  # No more lines to process
   defp build_uncovered_groups([], groups), do: groups
 
   # Start a new group with this uncovered line
@@ -47,37 +43,31 @@ defmodule CoverageReporter.UncoveredLineGrouper do
 
   # Hit a covered, non-fuzzy line - check if we should include it for fuzziness
   defp collect_group([{line_number, content, false, false} | rest], current_group) do
-    case look_ahead_for_uncovered(rest, 1) do
-      {true, _} ->
-        collect_group(rest, [line_number | current_group])
-
-      {false, _} ->
-        {Enum.reverse(current_group), [{line_number, content, false, false} | rest]}
+    if look_ahead_for_uncovered?(rest, 1) do
+      collect_group(rest, [line_number | current_group])
+    else
+      {Enum.reverse(current_group), [{line_number, content, false, false} | rest]}
     end
   end
 
   # Look ahead to see if there are uncovered lines within max_distance
-  defp look_ahead_for_uncovered(lines, distance) do
+  defp look_ahead_for_uncovered?(lines, distance) do
     case {lines, distance} do
       # No more lines to check
       {[], _} ->
-        {false, []}
+        false
 
       # Found an uncovered line within distance
       {[{_line_number, _content, true, _blank_line?} | _rest], distance} when distance <= 1 ->
-        {true, []}
+        true
 
       # Skip blank lines and continue looking
       {[{_line_number, _content, false, true} | rest], distance} when distance <= 1 ->
-        look_ahead_for_uncovered(rest, distance)
-
-      # Skip one covered line and continue looking
-      {[{_line_number, _content, false, false} | rest], distance} when distance < 1 ->
-        look_ahead_for_uncovered(rest, distance + 1)
+        look_ahead_for_uncovered?(rest, distance)
 
       # Distance exceeded or other cases
       _ ->
-        {false, []}
+        false
     end
   end
 end
